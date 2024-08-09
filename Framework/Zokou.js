9e9bb35a024287c40593f3898b2ebcf5c02433be
carl165 <167853149+Carl165@users.noreply.github.com>
2024-08-09 13:36:41 +0300

    𝗕𝗟𝗨𝗘-𝗕𝗘𝗘𝗧𝗟𝗘🐞

--- Framework/Zokou.js
+++ Framework/Zokou.js
@@ -1,1 +1,16 @@
-
+var tabCmds = [];
+let cm = [];
+function zokou(obj, fonctions) {
+    let infoComs = obj;
+    if (!obj.categorie) {
+        infoComs.categorie = "General";
+    }
+    if (!obj.reaction) {
+        infoComs.reaction = "🐞";
+    }
+    infoComs.fonction = fonctions;
+    cm.push(infoComs);
+    // console.log('chargement...')
+    return infoComs;
+}
+module.exports = { zokou, Module: zokou, cm };
